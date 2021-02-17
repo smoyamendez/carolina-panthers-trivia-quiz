05d4e6916fafd10cd4d2d84447a527570b435679
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-02-16 22:12:13 -0500

    added question variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 // Variables to link from HTML
+var startBtn = document.querySelector("#start");
+var questionsContainer = document.querySelector("#questionBox");
 
 // Variables for Quiz Questions
-
+var quizQuestions = [
+{Question 1: "Who was the Panther's first starting quarterback?", answers:["Tom Brady", "Joe Montana", "Frank Reich", "Warren Moon"], correct:"Frank Reich"},
+{Question 2: "Who caught the Panther's first touchdown?", answers:["Pete Metzelaars", "Don Beebe", "Bob Christian", "Reggie Jones"], correct:"Pete Metzelaars"},
+{Question 3: "What was the huddle/playbook name of the game-winning play in the double overtime marathon in Missouri?", answers:["Tom Brady", "Joe Montana", "Frank Reich", "Warren Moon"], correct:"Frank Reich"},
+{Question 4: "Which celebrity was the first player ever cut by the Panthers?", answers:["O.J. Simpson", "John Matuszak", "Bill Goldberg", "Terry Crews"], correct:"Bill Goldberg"},
+{Question 5: "Which RB/QB combo pioneered the now widely used 'wildcat' offense?", answers:["Deangelo Williams/Brett Basanez", "Jonathan Stewart/Cam Newton", "Nick Goings/Jake Delhomme", "Derrick Moore/Kerry Collins"], correct:"Deangelo Williams/Brett Basanez"},
+]
 // Timer function
 
 // 
