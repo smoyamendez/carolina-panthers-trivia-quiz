522bb87f5a057b835204ce5c93096eac26b34a98
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-02-18 00:02:07 -0500

    questions iterate when press StartBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,85 @@
 // Variables to link from HTML
 var startBtn = document.querySelector("#start");
 var questionsContainer = document.querySelector("#questionBox");
-var timeClock = document.querySelector("#timer");
 var instructions = document.querySelector("#instructions-container");
+var currentQuestion = 0;
+var questions = document.querySelector("#questions");
+var results = document.querySelector("#result");
 
-
-var answer1 = document.querySelector("#1");
-var answer2 = document.querySelector("#2");
-var answer3 = document.querySelector("#3");
-var answer4 = document.querySelector("#4");
 
 // Variables for Quiz Questions
 
 var quizQuestions = [
-{Question: "Who was the Panther's first starting quarterback?", answers:["Tom Brady", "Joe Montana", "Frank Reich", "Warren Moon"], correct:"Frank Reich"},
-{Question: "Who caught the Panther's first touchdown?", answers:["Pete Metzelaars", "Don Beebe", "Bob Christian", "Reggie Jones"], correct:"Pete Metzelaars"},
-{Question: "What was the huddle/playbook name of the game-winning play in the double overtime marathon in Missouri?", answers:["Even Janitor", "X-Clown", "Wildcat", "Statue of Liberty"], correct:"X-Clown"},
-{Question: "Which celebrity was the first player ever cut by the Panthers?", answers:["O.J. Simpson", "John Matuszak", "Bill Goldberg", "Terry Crews"], correct:"Bill Goldberg"},
-{Question: "Which RB/QB combo pioneered the now widely used 'wildcat' offense?", answers:["Deangelo Williams/Brett Basanez", "Jonathan Stewart/Cam Newton", "Nick Goings/Jake Delhomme", "Derrick Moore/Kerry Collins"], correct:"Deangelo Williams/Brett Basanez"},
+    { Question: "Who was the Panther's first starting quarterback?", answers: ["Tom Brady", "Joe Montana", "Frank Reich", "Warren Moon"], correct: "Frank Reich" },
+    { Question: "Who caught the Panther's first touchdown?", answers: ["Pete Metzelaars", "Don Beebe", "Bob Christian", "Reggie Jones"], correct: "Pete Metzelaars" },
+    { Question: "What was the huddle/playbook name of the game-winning play in the double overtime marathon in Missouri?", answers: ["Even Janitor", "X-Clown", "Wildcat", "Statue of Liberty"], correct: "X-Clown" },
+    { Question: "Which celebrity was the first player ever cut by the Panthers?", answers: ["O.J. Simpson", "John Matuszak", "Bill Goldberg", "Terry Crews"], correct: "Bill Goldberg" },
+    { Question: "Which RB/QB combo pioneered the now widely used 'wildcat' offense?", answers: ["Deangelo Williams/Brett Basanez", "Jonathan Stewart/Cam Newton", "Nick Goings/Jake Delhomme", "Derrick Moore/Kerry Collins"], correct: "Deangelo Williams/Brett Basanez" },
 ]
 
-// when start(call in event listener) Loop through answers and generate buttons in loop (use HTML DOM appendchild()) Method
+console.table(quizQuestions);
 
-function generateQuestions() {
-    var question = document.createElement("ul")
+
+
+
+//  for each answer create a button
+
+// for (var i = 0; i < quizQuestions[i].Question[i].answers[i].length; i++) {
+//     var answerEl = document.createElement ('button');
+//     answerEl.textContent = quizQuestions[i].answers[i];
+//     questionsContainer.append(answerEl);
+// }
+
+// Timer
+// var timeEl = document.querySelector("#timer");
+// var secondsLeft = 60;
+
+// function timerSet() {
+// // Sets interval
+// var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = "Time Left: " + secondsLeft;
+
+//     if(secondsLeft === 0) {
+//         // Stop game
+//         // alert game over
+//         // bring up end game
+//     }, 1000;
+// }
+
+// )
+// }
+
+
+function checkAnswer(event) {
+    console.log(event.target.textContent);
+    if (event.target.textContent !== quizQuestions[currentQuestion].correct){
+        alert("wrong");
+    }
+    currentQuestion++;
+    if (currentQuestion !== quizQuestions.length) {
+        showQuestion();
+    } else {
+        alert("Game Over");
+    }
 }
 
-displayAnswers();
-// Use a counter variable to keep track of what question I am on
-// for (var i = 0; i < question[count].answers.length; i++) {
-    //create button using js
-    // add data attribute to button that corresponds to answer position in array so I can check vs correct answer
-    //add button text
-    //append to the div I want to put the buttons in }
-    
-    
-    // start quiz with event listener function
-    startBtn.addEventListener("click", function() { 
-        
-        console.log("Start Quiz");
-        
-        // Timer function => needs to start timer when start clicked & end quiz and show score page when finished
-        
-        // function totalTime()
-        
-        
-        // function when timer is at nothing, then create X element and set it to this source in this area
-        // function sendMessage() {
-        //     timeClock.textContent = " ";
-        //     var imgEl = document.createElement("img");
-        //     imgEl.setAttribute("src", "images/image_1.jpg");
-        //     mainEl.appendChild(imgEl);
-            
-        // }
-        
-  })
+function showQuestion() {
+    questions.textContent = quizQuestions[currentQuestion].Question;
+    results.innerHTML = "";
+    for (let i = 0; i < quizQuestions[currentQuestion].answers.length; i++) {
+        const element = quizQuestions[currentQuestion].answers[i];
+        var newBtn = document.createElement('button');
+        newBtn.textContent = element;
+        console.log(newBtn, results);
+        newBtn.addEventListener("click", checkAnswer);
+        results.appendChild(newBtn);
+    }
+}
 
-  
+// start quiz with event listener function
+startBtn.addEventListener("click", function () {
+    showQuestion();
+    console.log("Start Quiz");
+})
+
