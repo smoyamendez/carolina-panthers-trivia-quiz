9017cda027fe6a8f9271ac9e4f069104c4640563
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-02-19 00:08:33 -0500

    set up links to highscore page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,11 @@ var questionsContainer = document.querySelector("#questionBox");
 var instructions = document.querySelector("#instructions-container");
 var currentQuestion = 0;
 var questions = document.querySelector("#questions");
+var answersBox = document.querySelector("#answers");
 var results = document.querySelector("#result");
 var gameOver = document.querySelector('#gameEnd');
 var timeEl = document.querySelector("#timer");
+var initialsEl = document.querySelector("#initials");
 var finalScoreEl = document.querySelector("#finalScore");
 var secondsLeft = 60;
 var timerInterval;
@@ -28,62 +30,105 @@ var quizQuestions = [
     function checkAnswer(event) {
         console.log(event.target.textContent);
         if (event.target.textContent !== quizQuestions[currentQuestion].correct){
-            alert("wrong");
+            wrong();
             secondsLeft = secondsLeft - 10;
+        }
+        if (event.target.textContent === quizQuestions[currentQuestion].correct) {
+            correct();
         }
         currentQuestion++;
         if (currentQuestion !== quizQuestions.length) {
             showQuestion();
         } else {
-            showScore();
+            endQuiz();
         }
     }
+    
+    // Show wrong function
+    function wrong() {
+        results.textContent = "INCORRECT";
+        setTimeout(function () {
+        results.textContent = "";
+    }, 1000);
+}
+    // Show correct function
+    function correct() {
+        results.textContent = "CORRECT";
+        setTimeout(function () {
+        results.textContent = "";
+    }, 1000);
+}
     
     // shows the Question from array in the div #questions & creates buttons for each answer
     function showQuestion() {
         questions.textContent = quizQuestions[currentQuestion].Question;
-        results.innerHTML = "";
+        answersBox.innerHTML = "";
         for (let i = 0; i < quizQuestions[currentQuestion].answers.length; i++) {
             const element = quizQuestions[currentQuestion].answers[i];
             var newBtn = document.createElement('button');
             newBtn.textContent = element;
-            console.log(newBtn, results);
+            console.log(newBtn, answersBox);
             newBtn.addEventListener("click", checkAnswer);
-            results.appendChild(newBtn);
+            answersBox.appendChild(newBtn);
+            newBtn.style.margin = "20px";
         }
     }
-    
-
-
-
-    
+        
 //  Changes the time Left to Final Score
-    function showScore() {
-            questions.style.display = "none";
-            results.style.display = "none";
-            gameEnd.style.display = "block";
-            timer.textContent = "Final Score: " + secondsLeft;
-            clearInterval(timerInterval);
+    function endQuiz() {
+        results.textContent = "";
+        clearInterval(timerInterval);
+        timer.textContent = "Time Left: " + 0;
+        questions.style.display= "none";
+        answersBox.style.display= "none";
+        
+        if (secondsLeft < 0) {
+            secondsLeft = 0;
+        }
+        gameOver.textContent = "All done!"
+        finalScoreEl.textContent = "Final Score: " + secondsLeft;
+        var initialsInput = document.createElement("INPUT");
+        var submitInitials = document.createElement('button');
+        var nameText = document.createElement("p");
+        nameText.textContent = "Enter Your Name";
+        submitInitials.href = "highscores.html";
+        submitInitials.textContent = "SUBMIT";
+        submitInitials.setAttribute("onclick", "window.location.href='highscores.html'");
+        initialsInput.setAttribute("type", "text");
+        initialsEl.appendChild(nameText);
+        initialsEl.appendChild(initialsInput);
+        initialsEl.appendChild(submitInitials);
+        submitInitials.onclick(submitIntitials.hef);
     }
     
+    // Go to high score page
+    function highScore() {
+
+    }
 // Timer Function
-    function startTimer() {timerInterval = setInterval(function() {
+    function startTimer() {
+        timerInterval = setInterval(function() {
         secondsLeft--;
-        timer.textContent = "Time left: " + secondsLeft;
+        timer.textContent = "Time Left: " + secondsLeft;
     
         if(secondsLeft === 0) {
           clearInterval(timerInterval);
-          showScore();
+          endQuiz();
         }
       }, 1000);
     }
 
-    // start quiz with event listener function
-    startBtn.addEventListener("click", function () {
+    function startQuiz () {
+        
         startBtn.style.display= "none";
         instructions.style.display= "none";
-
+    
         showQuestion();
         startTimer();
-    })
+    }
+
+
+    // start quiz with event listener function
+    startBtn.addEventListener("click", startQuiz) 
+
 
